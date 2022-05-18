3eb2713b0173c0577205a3d43402afcaff39167e
Dmytro Kovalenko <dimitri.yu.kovalenko@gmail.com>
2022-05-18 21:49:26 +0300

    square method

--- main.cpp
+++ main.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
-
+#include <string>
+#include <cmath>
+template <typename T> 
+int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+void Nevyazka_idk_know_how_to_call_it(std::vector<std::vector<double> >& matrix, std::vector<double>& b, std::vector<double>& val){
+    const int SIZE = b.size();
+    std::vector<double> r;
+    int i = 0, j = 0;
+    for(i = 0; i < SIZE; i++){
+        double sum = 0;
+        for(j = 0; j < SIZE; j++){
+            sum+=matrix[i][j] * val[j];
+        }
+        r.push_back(sum - b[i]);
+        j = 0;
+        
+    }
+    std::cout << "\nNevyazka:\n";
+    for(auto i : r){
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+}
+void CalculateSD(std::vector<std::vector<double> >& S, std::vector<std::vector<double> >& D, std::vector<std::vector<double> >& A, double& determinant){
+    S[0][0] = sqrt(abs(A[0][0]));
+    D[0][0] = sgn(A[0][0]);
+    for(int i = 1; i < 4; i++){
+        double sum = 0;
+        for(int p = 0; p < i; p++){
+            sum+= pow(S[p][i], 2) * D[p][p];
+        }
+        D[i][i] = sgn(A[i][i] - sum);
+        S[i][i] = sqrt(abs(A[i][i] - sum));
+        if(i!=3){
+            for(int j = i+1; j <4; j++){
+                double sum1 = 0;
+                for(int p = 0; p < i; p++){
+                    sum1 += S[p][i] * D[p][p] * S[p][j];
+                }
+                S[i][j] = (A[i][j] - sum1) / (D[i][i] * S[i][i]);
+            }
+        }
+    }
+    for(int i = 0; i < 4; i++){
+        determinant *= D[i][i] * pow(S[i][i], 2);
+    }
+    std::cout << "Determinant = " << determinant << std::endl;
+}
 void CreateMatrix(std::vector<std::vector<double> >& matrix, std::vector<double>& B_matrix, const int N){
     int i = 0;
     int j = 0;
@@ -28,25 +77,62 @@ void CreateMatrix(std::vector<std::vector<double> >& matrix, std::vector<double>
     std::cout << '\n';
     std::cout <<"Done! \n";
 }
-void Nevyazka_idk_know_how_to_call_it(std::vector<std::vector<double> >& matrix, std::vector<double>& b, std::vector<double>& val){
-    const int SIZE = b.size();
-    std::vector<double> r;
-    int i = 0, j = 0;
-    for(i = 0; i < SIZE; i++){
-        double sum = 0;
-        for(j = 0; j < SIZE; j++){
-            sum+=matrix[i][j] * val[j];
+void SquareMethod(){
+    std::vector<std::vector<double> > Matrix (4, std::vector<double>(4,0));
+    std::vector<std::vector<double> > S (4, std::vector<double>(4,0));
+    std::vector<std::vector<double> > D (4, std::vector<double>(4,0));
+    double determinant = 1;
+    double eps = 0.0001;
+
+    std::vector<double> B_vec(4,0);
+    CreateMatrix(Matrix, B_vec, 4);
+    CalculateSD(S, D, Matrix, determinant);
+
+    std::vector<std::vector<double> > St(4, std::vector<double>(4,0));
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            St[i][j] = S[j][i];
         }
-        r.push_back(sum - b[i]);
-        j = 0;
-        
     }
-    std::cout << "\nNevyazka:\n";
-    for(auto i : r){
-        std::cout << i << " ";
+
+    std::vector<std::vector<double> > StD = St;
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            StD[i][j] = 0.0;
+            for(int k = 0; k < 4; k++){
+                StD[i][j] += St[i][k] * D[k][j];
+            }
+        }
+    }
+
+    std::vector<double> y(4,0);
+    y[0] = (B_vec[0] / StD[0][0]);
+    y[1] = ((B_vec[1] - y[0] * StD[1][0]) / StD[1][1]);
+    y[2] = ((B_vec[2] - y[0] * StD[2][0] - y[1] * StD[2][1]) / StD[2][2]);
+    y[3] = ((B_vec[3] - y[0] * StD[3][0] - y[1] * StD[3][1] - y[2] * StD[3][2]) / StD[3][3]);
+    std::cout << "y vector:\n";
+    for(auto i : y){
+        std::cout << i << '\t';
     }
     std::cout << "\n";
+
+    std::vector<double> x(4,0);
+    x[3] = (y[3] / S[3][3]);
+    x[2] = ((y[2] - x[3] * S[2][3]) / S[2][2]);
+    x[1] = ((y[1] - x[3] * S[1][3] - x[2] * S[1][2]) / S[1][1]);
+    x[0] = ((y[0] - x[3] * S[0][3] - x[2] * S[0][2] - x[1] * S[0][1]) / S[0][0]);
+    std::cout << "x vector:\n";
+    for(auto i : x){
+        std::cout << i << '\t';
+    }
+    std::cout << "\n";
+
+    Nevyazka_idk_know_how_to_call_it(Matrix, B_vec, x);
+
 }
+
+
 void SeidelMethod(std::vector<std::vector<double> >& a,
                 std::vector<double>& B_matrix, const int n){
     for(int i = 0; i < n; i++){
@@ -103,12 +189,16 @@ int main(){
     std::cout.precision(6);
     std::cout.setf(std::ios::fixed);
     int N;
+    std::string answer;
+    std::cout << "Put S for SQRT or SS for Seidel: ";
+    std::cin >> answer;
     std::cout << "Put N size for matrix:\n";
     std::cin >> N;
     std::vector<double> B_matrix(N, 0);
     //std::cout << B_matrix.size() << std::endl;
     std::vector< std::vector<double> > matrix(N, std::vector<double>(N, 0));
     CreateMatrix(matrix, B_matrix, N);
-    SeidelMethod(matrix, B_matrix, N);
+    if(answer == "SS") SeidelMethod(matrix, B_matrix, N);
+    if(answer == "S") SquareMethod();
     return 0;
 }
